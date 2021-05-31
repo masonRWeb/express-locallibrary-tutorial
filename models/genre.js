31a3c15770aea1ce4c7833564ae38362575f21ce
MasonRingbom <62159435+MasonRingbom@users.noreply.github.com>
2021-05-30 22:30:02 -0700

    First version of application moved into GitHub

---
+++ models/genre.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+{
+    name: {type: String, required: true, maxLength: 100, minLength: 3}
+}
+);
+
+//declare virtual
+GenreSchema
+.virtual('url')
+.get(function() {
+    return '/catalog/genre/' + this._id;
+});
+
+//export model
+module.exports = mongoose.model('Genre', GenreSchema);
